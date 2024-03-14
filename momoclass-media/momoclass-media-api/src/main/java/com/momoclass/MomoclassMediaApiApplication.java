1b650e63abf9ba72b0b2627c906fde0a4b31c1c7
Yonagi <1809266700@qq.com>
2024-03-14 10:45:30 +0800

    day06, 新搭建媒体服务

---
+++ momoclass-media/momoclass-media-api/src/main/java/com/momoclass/MomoclassMediaApiApplication.java
@@ -0,0 +1,13 @@
+package com.momoclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomoclassMediaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MomoclassMediaApiApplication.class, args);
+    }
+
+}
